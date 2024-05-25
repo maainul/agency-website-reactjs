61190d23d6b553d34e3013ab672ed94f5ed3a390
Md.Mainul Hasan <mdevhasan@gmail.com>
2024-05-25 12:14:22 +0600

    blog cta and footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
+import Blog from './components/Blog/Blog';
+import CTA from './components/CTA/CTA';
+import Footer from './components/Footer/Footer';
 import HeaderComp from './components/HeaderComp/HeaderComp';
 import Number from './components/Number/Number';
+import Projects from './components/Projects/Projects';
 import Services from './components/Services/Services';
+import Testimonials from './components/Testimonials/Testimonials';
 
 function App() {
   return (
    <>
-   <HeaderComp/>
-    <Services/>
-    <Number/>
+      <HeaderComp/>
+      <Services/>
+      <Number/>
+      <Projects/>
+      <Testimonials/>
+      <Blog/>
+      <CTA/>
+      <Footer/>
    </>
   );
 }
---
+++ src/components/CTA/CTA.js
@@ -0,0 +1,21 @@
+
+
+function CTA(){
+    return(
+        <>
+            <section class="cta container">
+                <div class="ctn-container">
+                    <div class="cta__getStarted">
+                        <div class="cta__text">
+                            <h1>Get Connected With TechBees</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                        <a href="#" class="ctnBtn">Get Started</a>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default CTA
