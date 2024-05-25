4cca7d4a05d89be89e5407a4117c64e429fadd66
Md.Mainul Hasan <mdevhasan@gmail.com>
2024-05-25 11:19:16 +0600

    One Style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import './App.css';
-import Blog from './components/Blog/Blog';
-import CTA from './components/CTA/CTA';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/HeaderSection';
+import HeaderComp from './components/HeaderComp/HeaderComp';
 import Number from './components/Number/Number';
-import Project from './components/Project/Project';
 import Services from './components/Services/Services';
-import Testimonials from './components/Testimonials/Testimonials';
 
 function App() {
   return (
    <>
-      <Header />
-      <Services />
-      <Number/>
-      <Project/>
-      <Testimonials />
-      <Blog />
-      <CTA/>
-      <Footer/>
+   <HeaderComp/>
+    <Services/>
+    <Number/>
    </>
   );
 }
--- src/components/HeaderComp/HeaderComp.js
+++ src/components/HeaderComp/HeaderComp.js
@@ -1,51 +1,53 @@
-import './HeaderSection.css'
-
-function HeaderSection(){
+function HeaderComp(){
     return (
         <>
-            <header>
-                <div className="navbar">
-                <div className="theme">
+          <header>
+                <div>
+                <div className="navbar" >
+                    <div className="theme">
                     <input type="checkbox" name id="checkbox" className="checkbox" />
                     <label className="label" htmlFor="checkbox">
-                    <div className="ball" />
+                        <div className="ball" />
                     </label>
-                </div>
-                <img src={`${process.env.PUBLIC_URL}/images/logo.png`} alt="Logo" />
-                <ul>
+                    </div>
+                    <img src="images/logo.png" alt='logo' />
+                    <ul>
                     <li><a href="#home">Home</a></li>
                     <li><a href="#home">About</a></li>
                     <li><a href="#home">Services</a></li>
                     <li><a href="#home">Portfolio</a></li>
                     <li><a href="#home">Blog</a></li>
                     <li><a href="#home">Contact Us</a></li>
-                </ul>
+                    </ul>
+                    {/* <div id="mobile">
+                    <i id="bar" class="fa fa-fa-outdent"></i>
+                </div> */}
                 </div>
                 <div className="container hero">
-                <div className="hero__content">
+                    <div className="hero__content">
                     <div className="hero__content__description">
-                    <h1>We are startup<br /> creative agency</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni soluta libero atque, repellendus
+                        <h1>We are startup<br /> creative agency</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni soluta libero atque, repellendus
                         eos
                         ut
                         consequatur obcaecati amet nobis fugit eum, totam inventore vero aperiam laborum voluptatum,
                         maxime
                         quam
                         eligendi?</p>
-                    <div className="btn-group">
+                        <div className="btn-group">
                         <button className="btn learnmore">learn More</button>
                         <button className="btn">Contact Us</button>
-                    </div>
+                        </div>
                     </div>
                     <div className="hero__image">
-                    <img src={`${process.env.PUBLIC_URL}/images/hero-img.svg`} alt="Logo" />
+                        <img src="images/hero-img.svg" alt='hero'/>
+                    </div>
                     </div>
                 </div>
                 </div>
-                </header>
+            </header>
         </>
     )
-
 }
 
-export default HeaderSection
+export default HeaderComp
