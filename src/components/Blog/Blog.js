cd1a42b51f1eb929ed62842768ea1adc96de9ea8
maainul <mainul080@yahoo.com>
2024-05-26 18:45:03 +0600

    update

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -4,7 +4,7 @@ function Blog() {
         <>
             <section className="blog container section-gap">
                 <div className="single__blog">
-                    <img src="images/blog1.png" alt='blog' className="blog__img" />
+                    <img src="images/blog1.png" alt='blog 1' className="blog__img" />
                     <div className="blog__desc">
                         <h3>Powerful App for tracking daily activities</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, dignissimos. Cum autem itaque
@@ -13,7 +13,7 @@ function Blog() {
                     </div>
                 </div>
                 <div className="single__blog">
-                    <img src="images/blog2.png" alt='blog' className="blog__img" />
+                    <img src="images/blog2.png" alt='blog 2' className="blog__img" />
                     <div className="blog__desc">
                         <h3>Powerful App for tracking daily activities</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, dignissimos. Cum autem itaque
